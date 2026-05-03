45ad692a0441c2f0f7a29ebdaa7427b6641e9740
Pavan2398 <bodlapatipavan@gmail.com>
2026-05-03 11:55:57 +0530

    debug: add global error handler

--- server/server.js
+++ server/server.js
@@ -100,6 +100,12 @@ const startServer = async () => {
     });
   });
 
+  // Error handling middleware
+  app.use((err, req, res, next) => {
+    console.error('SERVER ERROR:', err.stack);
+    res.status(500).send('Internal Server Error');
+  });
+
   const PORT = process.env.PORT || 5000;
 
   app.listen(PORT, () => {
